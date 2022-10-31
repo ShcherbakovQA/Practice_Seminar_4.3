2a2bb36313afd2db9b1b40ac1df3183ddab1762c
Alexander <alex17.05.84@yandex.ru>
2022-10-31 16:43:06 +0300

    Работа с массивами

---
+++ Exercise_04/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пример работы с массивами на семинаре.
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr [i] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr [i] );
+    }
+}
+
+int[] array = new int[8];
+// int [] array = new int {1,0,10};
+
+FillArray(array); 
+PrintArray(array); 
+
+
